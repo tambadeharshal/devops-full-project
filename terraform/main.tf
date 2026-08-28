e28dd9c8296224cfdb8396984bb47b2e187a5473
India <India@gmail.com>
2026-08-28 16:09:32 +0530

    Add Azure DevOps CI pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,3 +17,30 @@ resource "azurerm_resource_group" "devops" {
   name     = "rg-devops-full-project"
   location = "Central India"
 }
+
+resource "azurerm_container_registry" "acr" {
+  name = "acrdevopsfulltf2026"
+  resource_group_name = azurerm_resource_group.devops.name
+  location            = azurerm_resource_group.devops.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-devops-full-project"
+
+  location            = azurerm_resource_group.devops.location
+  resource_group_name = azurerm_resource_group.devops.name
+
+  dns_prefix = "aks-devops-full"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
